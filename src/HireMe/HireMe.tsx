532d3ed506cd1212a8fba6fa706589e4f246e77c
Dragon <dragon@skullyflower.com>
2023-08-14 15:15:22 -0700

    updates

--- src/HireMe/HireMe.tsx
+++ src/HireMe/HireMe.tsx
@@ -7,9 +7,9 @@ const linksArray = [
     href: 'https://www.linkedin.com/in/skullyflower',
   },
   { name: 'GitHub', href: 'https://www.github.com/skullyflower' },
-  { name: 'Resumé', href: '/DragonMessmerResume.pdf' },
+  { name: 'Resumé', href: '/DragonMessmerwebResume.pdf' },
   { name: 'Hello Calculator', href: '/calc' },
-  { name: 'Skullyflower', href: 'https://www.skullyflower.com' },
+  { name: 'Skully Flower', href: 'https://www.skullyflower.com' },
 ];
 
 type SlideLinkType = { name: string; href: string };
@@ -32,58 +32,51 @@ export const HireMe = () => {
         <div className='content'>
           <h1>About me, Dragon Messmer.</h1>
           <div className={textExpanded ? 'description expanded' : 'description'}>
+            <h3 style={{ textAlign: 'center' }}>An adaptable and well rounded software engineer</h3>
             <p>
-              For over a decade, I&apos;ve been working remotely as a front-end software engineer.
-              My specialty is ReactJS. I enjoy writing original code, and creating solutions to
-              those problems that haven&apos;t been solved yet.
+              I have finally abandoned the impostor syndrome. I can learn anything and can apply
+              what I learn in a professional setting, rather quickly.
+            </p>
+            <p>
+              I&apos;ve been learning and applying new languages and methodologies since my first
+              tech gig. I barely knew HTML before I had to start scripting in PHP. The next job
+              required Javascript which is a very different language from PHP.
             </p>
             {textExpanded && (
               <>
                 <p>
-                  The front-end is my jam, but I am full-stack capable, and can update an API or SQL
-                  table. I&apos;ve always got the purpose, connected systems, and the end users of
-                  what I&apos;m working on in mind. It makes me better at communicating technical
-                  subjects to non-engineers.
+                  When I started at Unite, I&apos;d never used a Windows computer (I was Mac/ unix)
+                  was insecure about my Javascript knowledge, and had never even heard of C# or
+                  asp.net, but was quickly up and running in it all, and soon became the dev most
+                  requested by the pickiest clients.
                 </p>
                 <p>
-                  I&apos;m a natural. A few friends and I all got into web creation at the same
-                  time, but I am the only one still writing code, and I still love to do it. From
-                  the very start of my career, I&apos;ve been learning new languages and techniques,
-                  and been quickly applying them. My first job started building static HTML pages
-                  out of images in tables, but I quickly figured out how to create reusable, data
-                  driven, PHP templates and alleviate the repetitious work of creating the same
-                  document over and over. Although today I do my development work primarily using
-                  NodeJs and the cloud, my personal computer always has an apache web server running
-                  on it as a staging environment.
+                  Then at Homepoint I got the opportunity to get into React and have come into
+                  myself. I love the way React lends itself to clean, organized reusable code. Not
+                  that you cannot make a mess of it. I wonder where my next position will bring me?
+                  Ruby on Rails perhaps?
                 </p>
                 <p>
-                  Since then I&apos;ve built sites and apps, with various iterations of php and
-                  .net, Javascript, created custom frameworks and systems, refactored and reskinned
-                  others, and used many more. Through the years I&apos;ve built up my CSS,
-                  Javascript, REST, and Object Oriented coding patterns knowledge and skill. But the
-                  landscape is always changing, and there is always something new to learn and
-                  I&apos;m still open and able to learn. Every time I learn something particularly
-                  interesting or useful my site, skullyflower.com, gets refactored to use it.
+                  I used to become so scared and defensive when asked to do something outside my
+                  comfort zone. Now I&apos;ve learned to ask questions, look things up, and ask for
+                  help.
                 </p>
                 <p>
-                  In the last 6 or so years, I&apos;ve really fallen in love with React. It&apos;s a
-                  joy to work in, and makes it easy to separate concerns, reuse and organize code.
+                  I love to share knowledge. I write decent documentation. I&apos;m still a little
+                  shy and have a difficult time posting my writing and ideas into the world at
+                  large, but in the context of a company, where there is a general agreement or
+                  I&apos;ve been assigned as the expert, I&apos;ll keep records and make sure that
+                  others understand what they need to know.
                 </p>
-                <p>
-                  Early in my career, I worked primarily alone, but lately have benefitted from
-                  working in teams of 5-10 devs, writing and receiving code reviews and otherwise
-                  collaborating.
-                </p>
-                <p>When I can, I draw monsters and comics. I also occasionally paint.</p>
               </>
             )}
-            <p
-              style={{ fontSize: '1em', textAlign: 'right', cursor: 'pointer' }}
-              onClick={() => setTextExpaned(!textExpanded)}
-            >
-              {textExpanded ? '... Show less' : 'Show more ...'}
-            </p>
           </div>
+          <p
+            style={{ fontSize: '1em', textAlign: 'right', cursor: 'pointer' }}
+            onClick={() => setTextExpaned(!textExpanded)}
+          >
+            {textExpanded ? '... Show less' : 'Show more ...'}
+          </p>
           <div className='links'>
             {linksArray.map((link) => (
               <SlideLink key={link.name} name={link.name} href={link.href} />
