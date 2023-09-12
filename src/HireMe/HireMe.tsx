05181d8a29d2dd66246e842e8dea858209fbf153
Dragon <dragon@skullyflower.com>
2023-09-12 11:02:17 -0700

    updated about me

--- src/HireMe/HireMe.tsx
+++ src/HireMe/HireMe.tsx
@@ -17,7 +17,7 @@ type SlideLinkType = { name: string; href: string };
 const SlideLink = ({ name, href }: SlideLinkType) => {
   return (
     <div className='slideLink'>
-      <a href={href} target={name}>
+      <a href={href} target={name} rel='noopener'>
         {name}
       </a>
     </div>
@@ -40,33 +40,51 @@ export const HireMe = () => {
               with anything. In fact, that customer focus/product mindset is one of her biggest
               strengths. She was often requested by name by our professional services customers
               because of her attention to detail and focus on helping the customer achieve their
-              vision.&rdquo; - <i>Aaron Witt, engineering leader</i>
+              vision.&rdquo; -{' '}
+              <i>
+                <a
+                  href='https://www.linkedin.com/in/aaron-witt-1455783/'
+                  target='aaron'
+                  rel='noopener'
+                >
+                  Aaron Witt, engineering leader
+                </a>
+              </i>
             </p>
             {textExpanded && (
               <>
                 <p>
+                  My passion is bringing excellent designs to life with real data. I work well with
+                  in cross functional teams, boldly and tenaciously meeting the challenges of good
+                  UX design or complex data manipulations. I have experience creating fully
+                  responsive sites from scratch in pure css and html, and also with the aid of UI
+                  frameworks. I have a record of delighting impossible to please clients.
+                </p>
+                <p>
+                  A child of chaotic circumstances, I&apos;m the first in my family to have a
+                  professional career. I enjoy the quiet control of being able to make functioning
+                  machines out of typed script. When I&apos;m not coding, I&apos;m creating
+                  creatures and worlds in ink and paper. Socially, I&apos;m compassionate and
+                  supportive, but also quick to laugh and joke around.
+                </p>
+                <p>
+                  Professionally, I have finally abandoned the impostor syndrome. I&apos;ve proven I
+                  can learn anything and apply it in a professional setting, rather quickly. I ask
+                  questions, look things up, and ask for help.
+                </p>
+                <p>
                   I&apos;ve been learning and applying new languages and methodologies since my
                   first tech gig. I barely knew HTML before I had to start scripting in PHP. The
-                  next job required Javascript which is a very different language from PHP.
+                  next job required Javascript which is a very different language from PHP. Since
+                  then I&apos;ve worked in C#, angularJs, JQuery, and other languages and
+                  frameworks, up and down the stack.
                 </p>
                 <p>
-                  I have finally abandoned the impostor syndrome. I&apos;ve proven I can learn
-                  anything and apply it in a professional setting, rather quickly. I ask questions,
-                  look things up, and ask for help, instead of becoming scared or defensive when
-                  asked to do something outside my comfort zone.
+                  Then at Homepoint I had the opportunity to start building with React and have come
+                  to love the way React lends itself to clean, organized reusable code. (Not that
+                  you cannot make a mess of it.){' '}
                 </p>
-                <p>
-                  When I started at UniteU, I&apos;d never used a Windows computer (I was Mac/ unix)
-                  was insecure about my Javascript knowledge, and had never even heard of C# or
-                  asp.net, but was quickly up and running in it all, and soon became the dev most
-                  requested by the pickiest clients.
-                </p>
-                <p>
-                  Then at Homepoint I got the opportunity to start building with React and have come
-                  into myself. I love the way React lends itself to clean, organized reusable code.
-                  (Not that you cannot make a mess of it.) I wonder where my next position will
-                  bring me? Ruby on Rails perhaps?
-                </p>
+                <p>I wonder where my next position will bring me? Ruby on Rails perhaps?</p>
               </>
             )}
           </div>
